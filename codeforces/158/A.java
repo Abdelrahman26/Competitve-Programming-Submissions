087ee136b4e2f32cf2d7948d5bb04cb36a7c4bd2
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2018-02-07 17:21:00 +0200

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/35003185

--- codeforces/158/A.java
+++ codeforces/158/A.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
-
 public class nextround {
-	public static void main(String[] args) {
-		Scanner input =new Scanner( System.in);
-		int n = input.nextInt();
-		int k = input.nextInt();
-		int [] x =new int [n];
-		int count =0;
-		for(int i=0;i<n;i++)
-		{
-			x[i]=input.nextInt();
-		}
-		int m = x[k-1];
-		for(int i=0;i<n;i++)
-		{
-			if(x[i]>=m&&x[i]>0)count++;
-		}
-System.out.println(count);
-	}
 
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner input =new Scanner(System.in);
+        
+        int n = input.nextInt();
+        int [] p = new int [n];
+        int k = input.nextInt();
+        int  count = 0  ;
+        for(int i =0 ; i<n;i++){
+        	p[i] = input.nextInt();
+        }
+        for (int i = 0 ; i<n;i++){
+        	if (p[i]==0){}
+        	else{
+        		if ( p[i] >= p[k-1] )
+        		count++;
+        	
+        }}
+System.out.println(count);
+}}
