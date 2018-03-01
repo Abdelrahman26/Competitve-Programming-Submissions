6b2be07ce46d465f296b564dc7808c34390a15b5
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2018-03-01 20:01:00 +0200

    Add solution for problem `A - Buy a Shovel` Link: https://codeforces.com/contest/732/problem/A Tags: brute force, constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/732/submission/35816488

---
+++ codeforces/732/A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class startup {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner input = new Scanner(System.in);
+	int price = input.nextInt();
+	int r     = input.nextInt();
+	int m     = 0;
+	for(int i =1 ;i!=0;i++){
+		m=price*i;
+		if((m-r)%10==0||m%10==0){
+			System.out.println(i);
+			
+			  break;
+			}
+	}
+	}
+
+}
