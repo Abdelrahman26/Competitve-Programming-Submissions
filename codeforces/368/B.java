8aaec9a7e9b2ebfcafb66e9f90d2629a9f6d5d50
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2018-08-01 22:43:00 +0200

    Add solution for problem `B - Sereja and Suffixes` Link: https://codeforces.com/contest/368/problem/B Tags: data structures, dp, *1100 Ref: https://codeforces.com/contest/368/submission/41097402

--- codeforces/368/B.java
+++ codeforces/368/B.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
-public class serga {
+public class Sereja {
 	public static void main(String[] args) {
-    Scanner input =new Scanner(System.in);
-    int n =input.nextInt();
-    int m =input.nextInt();
-    int [] x =new int [n];
-    for(int i=0;i<n;i++)
-    {
-        x[i] = input.nextInt();
-    }
-    boolean [] seq = new boolean [100002];
-    int [] sum = new int [n+1];
-    for(int i=n-1;i>=0;i--)
-    {
-    if(seq[x[i]]==true)
-    {
-    	 sum[i] =sum [i+1];
-    }	                
-    else
-    	{
-         sum [i] =sum[i+1]+1;
-    	 seq[x[i]]=true;
-    	}
-   
-    }
-    for(int i=1;i<=m;i++)
-    {
-    	int w =input.nextInt();
-    	System.out.println(sum[w-1]);
-    }
-	}
-
+Scanner input =new Scanner(System.in);
+int n =input.nextInt();
+int m =input.nextInt();
+int []   x=new int [n+1];
+for(int i=1;i<=n;i++)
+{
+	x[i]=input.nextInt();
 }
+int [] sum =new int [n+1];
+//int [] seq =new int [100002];
+  boolean [] seq =new boolean[100002];
+for(int i =n;i>0;i--)
+{
+	if(i==n)
+	{
+		sum[i]=1;
+		seq[x[i]]=true;
+	}
+	else 
+	{
+		if(seq[x[i]]==true)
+		{
+			sum[i]=sum[i+1];
+		}
+		else 
+		{
+			seq[x[i]]=true;
+			sum[i]=sum[i+1]+1;
+		}
+	}
+}
+for(int i=0;i<m;i++)
+{
+	int q =input.nextInt();
+	System.out.println(sum[q]);
+}
+
+	}}
