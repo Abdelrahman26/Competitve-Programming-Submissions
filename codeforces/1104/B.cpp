33eb90a353814440572e045c72b8edcb5a08986f
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-01-22 18:27:00 +0200

    Add solution for problem `B - Game with string` Link: https://codeforces.com/contest/1104/problem/B Tags: data structures, implementation, math, *1200 Ref: https://codeforces.com/contest/1104/submission/48758564

--- codeforces/1104/B.cpp
+++ codeforces/1104/B.cpp
@@ -3,39 +3,33 @@
 #include <iomanip>
 #include<cmath>
 #include<vector>
-#include<queue>
-#include<stack>
-//#include<deque>
 #include<string>
-#include<sstream>
-#include <sstream>
 #include <algorithm>
 using namespace std;
 #define loop(n)  for(int i=0;i<n;i++)
 #define endl '\n'
 typedef long long ll;
-template<typename t>
-string itos(t i){
- stringstream s;
- s << i;
- return s.str();
- }
 int main()
 {
 cin.tie(0);
 cin.sync_with_stdio(0);
-string x ;
-cin>>x;
-int cnt=0;
-stack<char>s;
-loop(x.size()){
-if(!s.empty()&&s.top()==x[i]){
-        s.pop();
-        cnt++;
-}
-else s.push(x[i]);
-}
-if(cnt%2!=0)cout<<"YES";
-else cout<<"NO";
+string y;
+cin>>y;
+int n = y.size();
+int count=0;
+for(int i=0;y[i+1]!='\n';){
+    if(y[i]==y[i+1]){
+        y.erase(y.begin() +i);
+        y.erase(y.begin() +(i));
+        count++;
+        if(i-1!=-1&&y[i-1]==y[i])
+           i--;
+       // else i--;
 
+    }
+    else i++;
+    if(i==y.size())break;
+}
+if(count%2==0)cout<<"NO";
+else cout<<"YES";
 }
