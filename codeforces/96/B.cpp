3c39954a0894ec6cd2182fb033fd7c601dd0247b
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-01-24 11:43:00 +0200

    Add solution for problem `B - Lucky Numbers (easy)` Link: https://codeforces.com/contest/96/problem/B Tags: binary search, bitmasks, brute force, *1300 Ref: https://codeforces.com/contest/96/submission/48879713

--- codeforces/96/B.cpp
+++ codeforces/96/B.cpp
@@ -4,13 +4,11 @@
 #include<cmath>
 #include<vector>
 #include<queue>
-#include<stack>
 //#include<deque>
 #include<string>
 #include<sstream>
 #include <sstream>
 #include <algorithm>
-#include <map>
 using namespace std;
 #define loop(n)  for(int i=0;i<n;i++)
 #define endl '\n'
@@ -21,25 +19,22 @@ string itos(t i){
  s << i;
  return s.str();
  }
-long long mn  =1e12 ;
-long long  x ;
-void lucky(long long a  , int c ){
-    if(a > 1e11) return ;
-    if(a>=x &&  c==0){
-        mn = min( mn , a) ;
-    }
-    lucky( a * 10 + 4 , c+1 );
-    lucky( a * 10 + 7 , c-1 );
-}
 int main()
 {
-cin.tie(0);
-cin.sync_with_stdio(0);
-cout.tie(0);
-    cin>>x;
-    lucky(0,0);
-    cout<<mn;
-    return 0;
-
+  int n;
+  cin>>n;
+  ll value,c;
+  queue<pair<ll ,ll> >q;
+  q.push(make_pair(0,0));
+  while(!q.empty()){
+     value = q.front().first;
+     c     = q.front().second;
+    q.pop();
+    if(value>=n&&c==0){
+        cout<<value;
+        return 0;
+    }
+    q.push(make_pair(value*10+ 4,c+1));
+    q.push(make_pair(value*10+ 7,c-1));
+  }
 }
-
