f21919dd1bc25a7123e25e77273ac4fc6593ed57
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-01-26 18:34:00 +0200

    Add solution for problem `B - Digital root` Link: https://codeforces.com/contest/1107/problem/B Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1107/submission/49010860

---
+++ codeforces/1107/B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+//#include <bits/stdc++.h>
+#include <iomanip>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<stack>
+//#include<deque>
+#include<string>
+#include<sstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+using namespace std;
+#define loop(n)  for(int i=0;i<n;i++)
+#define endl '\n'
+typedef long long ll;
+template<typename t>
+string itos(t i){
+ stringstream s;
+ s << i;
+ return s.str();
+ }
+int main()
+{
+cin.tie(0);
+cin.sync_with_stdio(0);
+//map<string,string>m_;
+//map<string,string>::iterator it;
+ll n,k,x;
+cin>>n;
+loop(n){
+cin>>k>>x;
+cout<<x+((k-1)*9)<<endl;
+}
+
+
+}
+
