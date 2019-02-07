0f5f419c4b0a97e328843489fade41e9f8d3ecc8
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-02-07 21:42:00 +0200

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/49606369

---
+++ codeforces/158/A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+//#include <bits/stdc++.h>
+#include <iomanip>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<stack>
+//#include<deque>
+#include<string>
+#include<sstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+using namespace std;
+#define loop(n)  for(int i=0;i<n;i++)
+#define endl '\n'
+typedef long long ll;
+template<typename t>
+string itos(t i){
+ stringstream s;
+ s << i;
+ return s.str();
+ }
+int cnt;
+int main()
+{
+cin.tie(0);
+cin.sync_with_stdio(0);
+cout.tie(0);
+int n,k,idx=1,q;
+cin>>n>>k;
+vector<int>v(n);
+loop(n){
+cin>>v[i];
+if(i==k-1)q=v[i];
+}
+loop(n){
+if(v[i]>=q&v[i]!=0)cnt++;
+else break;
+}
+cout<<cnt;
+}
+
+
