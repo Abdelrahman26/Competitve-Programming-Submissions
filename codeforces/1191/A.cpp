33ad5f00e3a3dbf0e3d99eadae0e4ee853bf5559
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-07-12 17:15:00 +0200

    Add solution for problem `A - Tokitsukaze and Enhancement` Link: https://codeforces.com/contest/1191/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/1191/submission/56908244

--- codeforces/1191/A.cpp
+++ codeforces/1191/A.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
     using namespace std;
     #define loop(n)  for(int i=0;i<n;i++)
     #define endl '\n'
@@ -17,20 +17,38 @@ string itos(t i){
  stringstream s;
  s << i;
  return s.str();
-}
-stoi()
+ }
+ stoi()
 strcpy(char_array, s.c_str());
  */
-int ans;
+ bool st [5];
     int main()
     {
         cin.tie(0);
         cin.sync_with_stdio(0);
         cout.tie(0);
-        int x;
-        cin>>x;
-        if(x%4==0)cout<<"1 A";
-        else if(x%4==1)cout<<"0 A";
-          else if(x%4==2)cout<<"1 B";
-            else if(x%4==3)cout<<"2 A";
+        int n;
+        cin>>n;
+        if(n%4==1)return cout<<0<<" "<<'A',0;
+        pair<char,int>p[5];
+        int y = n%4;
+        p[y].first = 10;p[y].second = 0;
+        y = (n+1)%4;
+            p[y].first = 10;p[y].second = 1;
+        y = (n+2)%4;
+            p[y].first = 10;p[y].second = 2;
+        if(p[1].first==10){
+            cout<<p[1].second<<" "<<'A';
+        }
+         else if(p[3].first==10){
+            cout<<p[3].second<<" "<<'B';
+        }
+        else if(p[2].first==10){
+            cout<<p[2].second<<" "<<'C';
+        }
+         else if(p[0].first==10){
+            cout<<p[0].second<<" "<<'D';
+        }
+
+
     }
