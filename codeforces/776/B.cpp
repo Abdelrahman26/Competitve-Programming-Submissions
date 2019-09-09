3e71352e94f6ef6ee156bab2c64eb088b9f6b116
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2019-09-09 19:48:00 +0200

    Add solution for problem `B - Sherlock and his girlfriend` Link: https://codeforces.com/contest/776/problem/B Tags: constructive algorithms, number theory, *1200 Ref: https://codeforces.com/contest/776/submission/60315715

---
+++ codeforces/776/B.cpp
@@ -0,0 +1,116 @@
+   //#include <bits/stdc++.h>
+    #include <algorithm>
+    #include <iostream>
+    #include <iomanip>
+    #include <cstdio>
+    #include <cstdlib>
+    #include <cmath>
+    #include <cstring>
+    #include <vector>
+    #include <string>
+    #include <climits>
+    #include <map>
+    #include <set>
+    #include <stack>
+    #include <queue>
+    #include <deque>
+    using namespace std;
+    #define loop(n)  for(int i=0;i<n;i++)
+    #define loop(n) for(int i=0;i<n;i++)
+    #define endl '\n'
+    #define MAXNODES 100000+9
+    #define PB push_back
+    #define MP make_pair
+    #define OnlineJudgex
+    #define F first
+    #define S second
+    #define EPS 1e-9
+    #define PI acos(-1.0)
+    #define  precision(n)        cout << fixed << setprecision(n)
+    #define clr(v,d ) memset(v, d, sizeof(v))
+    #define MOD (int)1e9 + 7
+    typedef long long ll;
+    typedef vector<int> vi;
+    typedef vector<pair<int,int> >vp;
+    using namespace std;
+    /*
+    string itos(t i){
+     stringstream s;
+     s << i;
+     return s.str();
+    }
+    stoi()
+    strcpy(char_array, s.c_str());
+     */
+    void init()
+    {
+        cin.tie(0);
+        cin.sync_with_stdio(0);
+        cout.tie(0);
+    #ifdef OnlineJudge
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    }
+    struct point{
+	double x, y;
+    point() {x = y = 0.0;}
+    point(double _x, double _y) : x(_x), y(_y) {}
+    bool operator < (point other) const {
+        if ((fabs(x - other.x) > EPS))
+            return x < other.x;
+        return y < other.y;
+    }
+        bool operator == (point other) const {
+        return fabs(x-other.x) < EPS && fabs(y-other.y) < EPS;
+    }
+    point operator + (const point & p) const { return point(x + p.x, y + p.y); }
+	point operator - (const point & p) const { return point(x - p.x, y - p.y); }
+    double dot(const point & p) const { return x * p.x + y * p.y; }
+	double cross(const point & p) const { return x * p.y - y * p.x; }
+    };
+    int dx[] = {1,-1,0,0};
+    int dy[] = {0,0,1,-1};
+    double dist(point p1, point p2){
+    return hypot(p1.x - p2.x, p1.y - p2.y);
+    }
+    bool preceed(int x,int y)
+    {
+       return x > y;
+    }
+    int dcmp(double a,double b)
+    {
+        if ( fabs(a-b)  <= EPS )return  0 ;
+        return 1;
+    }
+   const int N  = 2e2+2;
+   bool flag;
+    int main()
+    {
+        init();
+        int n;
+        cin>>n;
+        n++;
+        int x[n+1]= {0};
+        int ans = 1;
+        x[1]    = 1;
+        for(int i = 2;i <= n;i++)
+        {
+            if(x[i] == 0)
+            {
+                int k = 1;
+                x[i]  = 1;
+                for(int j=i*2;j<=n ;j+=i)
+                {
+                 if(x[j] == 0)   x[j] = k + 1;
+                    if(x[j]> ans)ans = x[j];
+                }
+            }
+
+        }
+        cout<<ans<<endl;
+        for(int i=2;i<=n;i++)
+        {
+            cout<<x[i]<<" ";
+        }
+        }
