36bc49b854effd9060c8f3f915620daeeab8568f
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-02-20 10:40:00 +0200

    Add solution for problem `A - Circle Metro` Link: https://codeforces.com/contest/1169/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1169/submission/71481740

---
+++ codeforces/1169/A.cpp
@@ -0,0 +1,58 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+
+int main()
+{
+   init();
+   int n,a,x,b,y;
+   cin >> n >> a >> x >> b >> y;
+   bool ok = 0;
+   if(a == b)ok = 1;
+   while(a != x && b != y)
+   {
+       if(a  == n)a = 1;
+       else a++;
+       if(b == 1)b = n;
+       else b--;
+       if(a == b)ok = 1;
+
+   }
+   if(ok)cout<<"YES";
+   else cout<<"NO";
+}
