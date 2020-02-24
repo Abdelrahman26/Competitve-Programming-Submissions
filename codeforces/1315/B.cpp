dc0d2143588ccaf9048cded577e24be59099e163
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-02-24 15:53:00 +0200

    Add solution for problem `B - Homecoming` Link: https://codeforces.com/contest/1315/problem/B Tags: binary search, dp, greedy, strings, *1300 Ref: https://codeforces.com/contest/1315/submission/71767270

---
+++ codeforces/1315/B.cpp
@@ -0,0 +1,69 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+/// <<------------------------------------------------------------------------------>>
+
+
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+      int t;
+	cin >> t;
+	while(t--){
+		int a, b, p;
+		cin >> a >> b >> p;
+		string s;
+		cin >> s;
+		ll ans = 0;
+		int n  = s.size();
+		int i  = n - 2 , j = n , idx = 1 + n;
+		while(i >= 0)
+        {
+            j = i;
+            while(j >= 0 && s[i] == s[j])j--;
+            ans += s[i] == 'A' ? a : b;
+            if(ans <= p)
+            {
+                idx  = j + 1;
+            }
+            i = j;
+        }
+        cout<<min(++idx,n)<<endl;
+	}
+}
