30ecdfd12fc0b4eed01556d68c4b069cc75ea14f
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-03-19 19:02:00 +0200

    Add solution for problem `A - Bad Ugly Numbers` Link: https://codeforces.com/contest/1326/problem/A Tags: constructive algorithms, number theory, *1000 Ref: https://codeforces.com/contest/1326/submission/73732619

---
+++ codeforces/1326/A.cpp
@@ -0,0 +1,64 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+/// <<------------------------------------------------------------------------------>>
+
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n == 1)
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        string x = "";
+        loop(n-1)
+          x +='9';
+        if((n * 9) % 9 == 0)
+        x += '8';
+        else x += '9';
+        cout<<x<<endl;
+    }
+}
