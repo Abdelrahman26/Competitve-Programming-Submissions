8d863703ef92b91da5a32ae2148629941e29a5d9
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-03-19 17:36:00 +0200

    Add solution for problem `B - Maximums` Link: https://codeforces.com/contest/1326/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1326/submission/73700540

---
+++ codeforces/1326/B.cpp
@@ -0,0 +1,56 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+/// <<------------------------------------------------------------------------------>>
+
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    int n;
+    cin >> n;
+    ll a[n] = {0},b[n],x[n] = {0};
+    loop(n)cin >> b[i];
+    ll mx = 0;
+    loop(n)
+    {
+        a[i] = b[i] + mx;
+        if(a[i] > mx)mx = a[i];
+    }
+    loop(n)cout<<a[i]<<" ";
+}
