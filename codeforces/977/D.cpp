8671d3165f323ea289138146765a46d46ec089e2
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-03-22 01:35:00 +0200

    Add solution for problem `D - Divide by three, multiply by two` Link: https://codeforces.com/contest/977/problem/D Tags: dfs and similar, math, sortings, *1400 Ref: https://codeforces.com/contest/977/submission/73923030

---
+++ codeforces/977/D.cpp
@@ -0,0 +1,73 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+/// <<------------------------------------------------------------------------------>>
+map<ll,bool>deg;
+map<ll,bool>gh;
+vector<ll>v;
+void dfs(ll value)
+{
+    v.push_back(value);
+    if(gh[2 * value]) dfs(2 * value);
+    if(value % 3 == 0 && gh[value / 3]) dfs(value / 3);
+}
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    int n;
+    cin >> n;
+    ll arr[n];
+    loop(n)
+    {
+        cin >> arr[i];
+        gh[arr[i]] = 1;
+        deg[arr[i] * 2] = 1;
+        if(arr[i] % 3 == 0)
+            deg[arr[i] / 3] = 1;
+    }
+    loop(n)
+    {
+        if(deg[arr[i]] == 0)
+        {
+            dfs(arr[i]);
+            break;
+        }
+    }
+    loop(v.size())cout<<v[i]<<" ";
+}
