1806ec9208075ef98f8c2198db776aeac5271761
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-03-29 20:42:00 +0200

    Add solution for problem `A - Snowball` Link: https://codeforces.com/contest/1099/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1099/submission/74760009

---
+++ codeforces/1099/A.cpp
@@ -0,0 +1,61 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+#ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+/// <<------------------------------------------------------------------------------>>
+
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    int w , h , u1 , u2 , h1 , h2;
+    cin >> w >> h >> u1 >> h1 >> u2 >> h2;
+    for(int i = h ;i >= 0 ;i--)
+    {
+        w += i;
+        if(i == h1)
+        {
+            w = max(0,w - u1);
+        }
+        if(i == h2)
+        {
+            w = max(0,w - u2);
+        }
+    }
+    cout<<w<<endl;
+}
