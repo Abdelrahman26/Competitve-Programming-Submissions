9e691914fa938440ba1de1610358dccd6828eecb
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-04-12 01:42:00 +0200

    Add solution for problem `C - k-Tree` Link: https://codeforces.com/contest/431/problem/C Tags: dp, implementation, trees, *1600 Ref: https://codeforces.com/contest/431/submission/76273404

---
+++ codeforces/431/C.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+//#define int long long
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgec
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+    #ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+ll S(ll x){ll s = 0;while(x){s += x % 10;x /= 10;}return s;} /// sum of digits.
+ll powe(ll a , ll n){if(n == 1)return a;return a * powe(a ,n-1);}
+ll com(int n,int m){ll z = 1;for(int i = 0; i < m;i++){z = z * ( n - i) / (i + 1) ;}return z;}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+int dr[] = {0,0,0,1,1,1,-1,-1,-1};
+int dc[] ={-1,0,1,-1,0,1,-1,0,1};
+/// <<------------------------------------------------------------------------------>>
+const int N =109 , MOD = int(1e9) + 7;
+ll dp[N][2];
+int n ,k ,d;
+ll solve(ll sum, bool state)
+{
+    if(sum == n && state)
+        return 1;
+    if(sum >= n)
+        return 0;
+    ll &rst = dp[sum][state];
+    if(~rst)return rst;
+    ll ans = 0;
+    for(int i = 1 ;i <= k ;i++)
+    {
+        ans += solve(sum + i ,(state || i >= d) ? 1:0);
+        ans %= MOD;
+    }
+    return rst = ans;
+}
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    cin >> n >> k >> d;
+    clr(dp,-1);
+    cout<<solve(0,0);
+}
