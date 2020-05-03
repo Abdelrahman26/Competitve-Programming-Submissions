c884790bc9a323513258c91608664f80c1e432ed
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-05-03 16:39:00 +0200

    Add solution for problem `C - Gas Pipeline` Link: https://codeforces.com/contest/1207/problem/C Tags: dp, greedy, *1500 Ref: https://codeforces.com/contest/1207/submission/78899792

---
+++ codeforces/1207/C.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+//#define int long long
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudgeg
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+    #ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+ll S(ll x){ll s = 0;while(x){s += x % 10;x /= 10;}return s;} /// sum of digits.
+ll powe(ll a , ll n){if(n == 1)return a;return a * powe(a ,n-1);}
+ll com(int n,int m){ll z = 1;for(int i = 0; i < m;i++){z = z * ( n - i) / (i + 1) ;}return z;}
+int fix (int a,int k){ return (a%k + k)%k; } // convert N mod to P mode
+int * LIS_Lengths(vector<int> a) { int* lis = new int[a.size()];int b[a.size()];int maxLength = 1, lowerBound;lis[0] = 1, b[0] = a[0];for (size_t i = 1; i < a.size(); i++) {lowerBound = lower_bound(b, b + maxLength, a[i]) - b;lis[i] = lowerBound + 1;if (lowerBound == maxLength)    b[maxLength++] = a[i];else    b[lowerBound] = a[i];}return lis;}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+int dr[] = {0,0,0,1,1,1,-1,-1,-1};
+int dc[] ={-1,0,1,-1,0,1,-1,0,1};
+/// <<------------------------------------------------------------------------------>>
+ll t , n , road , pill;
+string x;
+ll dp[200009][3];
+ll solve(int i , int he)
+{
+    if(i == n)
+    {
+        if(he == 1)return 0;
+        return 1e17;
+    }
+    ll &rst = dp[i][he];
+    if(~rst)
+        return rst;
+    ll ch1 = 1e17 , ch2 = 1e17;
+    if(x[i] == '1')
+    {
+        if(he == 1)
+        ch1 = 1e17;
+        else
+        ch1 = 2 * pill+ road + solve(i+1 , 2);
+    }
+    else
+    {
+        if(he == 1)
+        {
+            ch1 = solve(i+1,1) + pill + road;
+            ch2 = solve(i+1 , 2) + 2 * (pill + road);
+        }
+        else
+        {
+            ch1 = solve(i+1,2) + 2 * pill + road;
+            ch2 = solve(i+1 , 1) + pill + 2 * road;
+        }
+    }
+    return rst = min(ch1,ch2);
+}
+/// <<------------------------------------------------------------------------------>>
+int main()
+{
+    init();
+    cin >> t;
+    while(t--)
+    {
+      cin >> n >> road >> pill >> x;
+      clr(dp,-1);
+      cout<<solve(0,1) + pill<<endl;
+    }
+}
