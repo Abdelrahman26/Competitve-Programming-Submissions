d4d5653f20144033da529d6a6a9e9e969dd96355
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-06-28 22:58:00 +0200

    Add solution for problem `A - Romaji` Link: https://codeforces.com/contest/1008/problem/A Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/1008/submission/85407601

--- codeforces/1008/A.py
+++ codeforces/1008/A.py
@@ -1,3 +1,13 @@
+vowel ={'a', 'o', 'u', 'e', 'i'}
 s = input()
-print('YES'if s[-1] in 'aeioun' and all(s[i] in 'aeioun' or s[i + 1] in 'aeiou' for i in range (len(s) - 1))
-      else 'NO')
+a = len(s)
+s = s + 'b'
+ok = 1
+for i  in range(a) :
+    if(s[i] not in vowel and s[i] != 'n' ):
+        if(s[i+1] not in vowel):
+            ok = 0
+if ok:
+    print("YES")
+else:
+    print("NO")
