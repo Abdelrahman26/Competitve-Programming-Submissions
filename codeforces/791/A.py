c1d98f9fc0f480bc0e9bd23100c71c93fe8c647a
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-06-28 14:36:00 +0200

    Add solution for problem `A - Bear and Big Brother` Link: https://codeforces.com/contest/791/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/791/submission/85287231

--- codeforces/791/A.py
+++ codeforces/791/A.py
@@ -1,3 +1,10 @@
-from math import log
 a, b = map(int, input().split())
-print(int(log(b / a, 3/2)) + 1)
+cnt  = 0
+i = 1
+while i:
+    a *= 3
+    b *= 2
+    cnt+=1
+    if a > b:
+        print(cnt)
+        break
