4c0df860cb742a0f5728d3c0a9956cde1e7f4cbf
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-06-28 12:59:00 +0200

    Add solution for problem `A - Word` Link: https://codeforces.com/contest/59/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/59/submission/85280363

--- codeforces/59/A.py
+++ codeforces/59/A.py
@@ -1,2 +1,11 @@
 s = input()
-print([s.lower(), s.upper()][sum(x<='Z' for x in s) *2 > len(s)])
+o = 0; e = 0
+for i in s:
+    if(i.islower()):
+        o += 1
+    else:
+        e += 1
+if o >= e:
+    print(s.lower())
+else:
+    print(s.upper())
