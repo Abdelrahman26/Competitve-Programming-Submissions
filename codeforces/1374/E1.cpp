6a18b72b0b86a3dfdaf08b17b5bbebf710bc4679
Abdelrahman Abdelmoneam <rahmanemoneam@gmail.com>
2020-07-06 21:32:00 +0200

    Add solution for problem `E1 - Reading Books (easy version)` Link: https://codeforces.com/contest/1374/problem/E1 Tags: data structures, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1374/submission/86153364

---
+++ codeforces/1374/E1.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <climits>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+//#define int long long
+using namespace std;
+#define loop(n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define OnlineJudge5
+#define F first
+#define S second
+#define  precision(n)  cout << fixed << setprecision(n)
+#define clr(v,d ) memset(v, d, sizeof(v))
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef pair<int,pii> pnii; /// weighted grid
+typedef vector<pii>vii;
+void init()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+    #ifdef OnlineJudge
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+ll S(ll x){ll s = 0;while(x){s += x % 10;x /= 10;}return s;} /// sum of digits.
+ll powe(ll a , ll n){if(n == 1)return a;return a * powe(a ,n-1);}
+ll com(int n,int m){ll z = 1;for(int i = 0; i < m;i++){z = z * ( n - i) / (i + 1) ;}return z;}
+int fix (int a,int k){ return (a%k + k)%k; } // convert N mod to P mode
+int * LIS_Lengths(vector<int> a) { int* lis = new int[a.size()];int b[a.size()];int maxLength = 1, lowerBound;lis[0] = 1, b[0] = a[0];for (size_t i = 1; i < a.size(); i++) {lowerBound = lower_bound(b, b + maxLength, a[i]) - b;
+lis[i] = lowerBound + 1;if (lowerBound == maxLength)    b[maxLength++] = a[i];else    b[lowerBound] = a[i];}return lis;}
+bool is_prime(ll n){if(n==2) return true;if(n<2 || n%2 == 0)return false;for(int i=3; i * i <= n; i+=2){if(n % i == 0)return false;}return true;}
+ll LCM(ll a, ll b){return (a * b)/__gcd(a,b);}
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
+int dr[] = {0,0,0,1,1,1,-1,-1,-1};
+int dc[] ={-1,0,1,-1,0,1,-1,0,1};
+/// <<------------------------------------------------------------------------------>>
+
+/// <<----;------------------------------------------------------------------------->>
+int main()
+{
+    init();
+
+    int n,k;
+    cin >> n >> k;
+    vector<pair<int, pair<int, int > > >arr(n);
+    int A = 0 , B = 0;
+    vi x,y,z;
+    loop(n)
+    {
+        int t , a , b;
+        cin >> t >> a >> b;
+        arr[i].F = t;
+        arr[i].S.F=a;
+        arr[i].S.S=b;
+        if(a) A++;
+        if(b) B++;
+        if(a&&b) z.push_back(t);
+        else if(a) x.push_back(t);
+        else if(b)y.push_back(t);
+    }
+
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+    sort(z.begin(), z.end());
+
+    if(A < k || B < k) return cout << -1,0;
+
+    loop(x.size())
+    {
+       if(i)
+            x[i] += x[i - 1];
+    }
+    loop(y.size())
+    {
+       if(i)
+            y[i] += y[i - 1];
+    }
+    loop(z.size())
+    {
+       if(i)
+            z[i] += z[i - 1];
+    }
+    int sum = 2e9;
+    if(z.size() >= k) /// z is enough to form a full solution
+    {
+        sum = z[k-1]  ;
+    }
+    
+    int m  = min(x.size(),y.size()); /// z is empty
+    if(m >= k)
+        sum = min(sum,x[k-1] + y[k - 1]);
+    
+    for(int i = 0 ;i < z.size() ;i++)
+    {
+       if( (k - i - 2) < x.size() && (k - i - 2 ) < y.size())
+       {
+             int r =  z[i] + x[ k - i - 2] + y[k - i - 2];
+             sum = min(sum,r);
+       }
+    }
+
+    cout << sum << endl;
+}
